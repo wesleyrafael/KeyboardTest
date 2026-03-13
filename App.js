371f08c8642012aaeab31c5cf42c786909fb19d7
Wesley Rafael <rafael.0404@hotmail.com>
2026-03-13 12:19:02 -0300

    creating minimum reproductible example repo

--- App.js
+++ App.js
@@ -1,12 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { KeyboardAwareScrollView, KeyboardProvider, KeyboardStickyView, KeyboardToolbar } from 'react-native-keyboard-controller';
+
+const FOOTER_SIZE = 60;
 
 export default function App() {
   return (
+    <KeyboardProvider>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <KeyboardAwareScrollView style={styles.scrollView} bottomOffset={FOOTER_SIZE}>
+
+      <TextInput style={styles.input} />
+
+      <TextInput style={styles.input} />
+
+      <TextInput style={styles.input} />
+
+      <TextInput style={[styles.input, { borderColor: 'red'}]} keyboardType="number-pad" returnKeyType="next" />
+
+      <TextInput style={styles.input} />
+
+      <TextInput style={styles.input} />
+
+      <TextInput style={styles.input} />
+
+      <TextInput style={styles.input} />
+
+      <TextInput style={styles.input} />
+
+      <TextInput style={styles.input} />
+
+      <TextInput style={styles.input} />
+
+      <TextInput style={styles.input} />
+
+      <TextInput style={styles.input} />
+
+      <TextInput style={styles.input} />
+      </KeyboardAwareScrollView>
+
+      <KeyboardToolbar>
+        <KeyboardToolbar.Prev />
+        <KeyboardToolbar.Next />
+        <KeyboardToolbar.Done />
+      </KeyboardToolbar>
+
+      <KeyboardStickyView style={styles.stickyView}>
+        <View style={styles.buttonContainer}>
+          <Button title='Submit' />
+        </View>
+      </KeyboardStickyView>
     </View>
+    </KeyboardProvider>
   );
 }
 
@@ -15,6 +60,28 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: '40%',
   },
+  scrollView: {
+    flex: 1,
+    width: '100%',
+    paddingHorizontal: '10%',
+  },
+  input: {
+    height: 40,
+    borderWidth: 0.5,
+    borderColor: 'black',
+    width: '100%',
+    borderRadius: 4,
+    marginBottom: 20,
+  },
+  stickyView: {
+    height: 60, 
+    width: '100%',
+  },
+  buttonContainer: {
+    backgroundColor: 'black',
+    height: '100%'
+  }
 });
