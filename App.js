ccc29690a17d1fa8e6ffd3f5043888dd3cc0215d
Wesley Rafael <rafael.0404@hotmail.com>
2026-05-07 15:08:00 -0300

    updating reproduction example

--- App.js
+++ App.js
@@ -1,84 +1,93 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 import { KeyboardAwareScrollView, KeyboardProvider, KeyboardStickyView, KeyboardToolbar } from 'react-native-keyboard-controller';
+import { SafeAreaProvider, SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const FOOTER_SIZE = 60;
 
-export default function App() {
+const Bottom = () => {
+  const { bottom } = useSafeAreaInsets();
   return (
-    <KeyboardProvider>
-    <View style={styles.container}>
-      <KeyboardAwareScrollView style={styles.scrollView} bottomOffset={FOOTER_SIZE}>
+    <KeyboardStickyView 
+      style={styles.stickyView} 
+      offset={{ 
+        opened: bottom
+      }}
+    >
+      <View style={styles.buttonContainer}>
+        <Button title='Submit' />
+      </View>
+    </KeyboardStickyView>
+  );
+}
 
-      <TextInput style={styles.input} />
+export default function App() {
+  
+  return (
+    <SafeAreaProvider>
+      <KeyboardProvider>
+        <SafeAreaView style={styles.safeArea}>
+          <KeyboardAwareScrollView contentContainerStyle={styles.scrollView} bottomOffset={FOOTER_SIZE}>
 
-      <TextInput style={styles.input} />
+          <TextInput style={styles.input} />
 
-      <TextInput style={styles.input} />
+          <TextInput style={styles.input} />
 
-      <TextInput style={[styles.input, { borderColor: 'red'}]} keyboardType="number-pad" returnKeyType="next" />
+          <TextInput style={styles.input} />
 
-      <TextInput style={styles.input} />
+          <TextInput style={[styles.input, { borderColor: 'red'}]} keyboardType="number-pad" returnKeyType="next" />
 
-      <TextInput style={styles.input} />
+          <TextInput style={styles.input} />
 
-      <TextInput style={styles.input} />
+          <TextInput style={styles.input} />
 
-      <TextInput style={styles.input} />
+          <TextInput style={styles.input} />
 
-      <TextInput style={styles.input} />
+          <TextInput style={styles.input} />
 
-      <TextInput style={styles.input} />
+          <TextInput style={styles.input} />
 
-      <TextInput style={styles.input} />
+          <TextInput style={styles.input} />
 
-      <TextInput style={styles.input} />
+          <TextInput style={styles.input} />
 
-      <TextInput style={styles.input} />
+          <TextInput style={styles.input} />
 
-      <TextInput style={styles.input} />
-      </KeyboardAwareScrollView>
+          <TextInput style={styles.input} />
 
-      <KeyboardToolbar>
-        <KeyboardToolbar.Prev />
-        <KeyboardToolbar.Next />
-        <KeyboardToolbar.Done />
-      </KeyboardToolbar>
-
-      <KeyboardStickyView style={styles.stickyView}>
-        <View style={styles.buttonContainer}>
-          <Button title='Submit' />
-        </View>
-      </KeyboardStickyView>
-    </View>
-    </KeyboardProvider>
+          <TextInput style={styles.input} />
+          </KeyboardAwareScrollView>
+          <Bottom />
+          
+        </SafeAreaView>
+      </KeyboardProvider>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: { 
+    flex: 1
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'flex-start',
-    paddingTop: '40%',
   },
   scrollView: {
-    flex: 1,
-    width: '100%',
+    flexGrow: 1,
     paddingHorizontal: '10%',
   },
   input: {
     height: 40,
     borderWidth: 0.5,
     borderColor: 'black',
-    width: '100%',
     borderRadius: 4,
     marginBottom: 20,
   },
   stickyView: {
-    height: 60, 
-    width: '100%',
+    height: FOOTER_SIZE, 
   },
   buttonContainer: {
     backgroundColor: 'black',
